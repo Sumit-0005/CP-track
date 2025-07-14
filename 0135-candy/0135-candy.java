6be71e2e97d4b447777e37f0fd12417a68ed8a68
Sumit kakani <sumitkakani0011@gmail.com>
2025-07-14 15:18:11 +0530

    Time: 3 ms (74.58%), Space: 45.6 MB (86.84%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candy = new int[n];
+
+        Arrays.fill(candy, 1);
+
+        for (int i = 0; i < n - 1; i++) {
+            if (ratings[i + 1] > ratings[i]) {
+                candy[i + 1] = candy[i] + 1;
+            }
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            if (ratings[i - 1] > ratings[i] && candy[i - 1] <= candy[i]) {
+                candy[i - 1] = candy[i] + 1;
+            }
+        }
+
+        int sum = 0;
+        for (int i : candy) {
+            sum += i;
+        }
+        return sum;
+    }
+}

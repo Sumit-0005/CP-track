5457122eed1fb64348bd74e430c5a953477a30e8
Sumit kakani <sumitkakani0011@gmail.com>
2026-01-01 19:12:50 +0530

    Time: 0 ms (100%), Space: 43.8 MB (7.44%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i= n-1; i>=0; i--){
+            digits[i]++;
+            if(digits[i] == 10){
+                digits[i] = 0;
+            }else{
+                return digits;
+            }
+        }
+        int[] newdigits = new int[n+1];
+        newdigits[0] = 1;
+        return newdigits;
+    }
+}

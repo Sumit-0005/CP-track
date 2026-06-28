e9cfdca84c489fd3ef8a71ab18f6cb8e513ac4e2
Sumit kakani <sumitkakani0011@gmail.com>
2026-06-28 19:39:03 +0530

    Time: 4 ms (99.73%), Space: 48 MB (11.69%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return isPalindromeRange(s, start + 1, end) || isPalindromeRange(s, start, end - 1);
+            }
+            start++;  end--;
+        }
+        return true;
+    }
+    private boolean isPalindromeRange(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++; right--;
+        }
+        return true;
+    }
+}

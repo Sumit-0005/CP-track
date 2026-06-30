730285c303c17553ed842742a86565604a430c28
Sumit kakani <sumitkakani0011@gmail.com>
2026-06-30 16:03:56 +0530

    Time: 0 ms (100%), Space: 48.3 MB (30.82%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        ans[0] = binarySearch(nums, target, true);
+        ans[1] = binarySearch(nums, target, false);
+        return ans;
+    }
+    static int binarySearch(int[] nums, int target, boolean find){
+        int s = 0, e = nums.length-1;
+        int ans = -1;
+        while(s <= e){
+            int mid = s+(e-s)/2;
+            if(target > nums[mid])
+                s = mid+1;
+            else if(target < nums[mid])
+                e = mid-1;
+            else{
+                ans = mid;
+                if(find) e = mid-1;
+                else s = mid+1;
+            }
+        }
+        return ans;
+    }
+}
